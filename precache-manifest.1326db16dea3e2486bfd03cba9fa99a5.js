0a25609b7fdc881d7022bda2f7ec57a5697acd10
ZubkovAC <3y6kob90@gmail.com>
2021-03-02 21:29:17 +0300

    Updates

---
+++ precache-manifest.1326db16dea3e2486bfd03cba9fa99a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bbed04cc6dfb5af5706f89efe82dfa0",
+    "url": "/JS_HomeWorks_Ignat/index.html"
+  },
+  {
+    "revision": "cc95e4e590a9211a912c",
+    "url": "/JS_HomeWorks_Ignat/static/css/main.405a3ba2.chunk.css"
+  },
+  {
+    "revision": "9b328c7045ea5863aa44",
+    "url": "/JS_HomeWorks_Ignat/static/js/2.853ef354.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/JS_HomeWorks_Ignat/static/js/2.853ef354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc95e4e590a9211a912c",
+    "url": "/JS_HomeWorks_Ignat/static/js/main.552a4d2d.chunk.js"
+  },
+  {
+    "revision": "18a9f0ec71be6b1fef3b",
+    "url": "/JS_HomeWorks_Ignat/static/js/runtime-main.6ea48457.js"
+  }
+]);
